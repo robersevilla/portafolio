edde60a08c226d11f9a0b3373634ede22146f3e8
Roberto Sanchez <robersevilla@gmail.com>
2017-09-04 13:56:10 +0200

    Add portfolio-item

---
+++ src/app/components/portfolio-item/portfolio-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-item',
+  templateUrl: './portfolio-item.component.html',
+  styles: []
+})
+export class PortfolioItemComponent  {
+
+  constructor() { }
+
+
+}
