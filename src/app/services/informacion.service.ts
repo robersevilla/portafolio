b50f613bebf7dce2c013fdab74d725749c11af4e
Roberto Sanchez <robersevilla@gmail.com>
2017-09-05 13:57:07 +0200

    Add Service Team developers Firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -7,16 +7,30 @@ export class InformacionService {
 
   info:any={};
   cargada:boolean=false;
+  cargada_sobre_nosotros:boolean=false;
+  equipo:any[]=[];
 
   constructor(public http:Http) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+   }
 
-this.http.get("assets/data/info.pagina.json")
-        .subscribe(data=>{
-          console.log(data.json());
-          this.cargada=true;
-          this.info=data.json();
-        })
+   public carga_info(){
+     this.http.get("assets/data/info.pagina.json")
+             .subscribe(data=>{
+               this.cargada=true;
+               this.info=data.json();
+             })
+   }
 
+public  carga_sobre_nosotros(){
+
+  this.http.get("https://paginaweb-fde42.firebaseio.com/equipo.json")
+          .subscribe(data=>{
+            console.log(data.json());
+            this.cargada_sobre_nosotros=true;
+            this.equipo=data.json();
+          })
 
    }
 
